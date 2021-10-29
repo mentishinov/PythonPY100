23c8fa647ff6b10027200ab2432efe5172186e6b
alemen <t@.ru>
2021-10-29 21:18:39 +0300

    классная работа 29.10

--- Занятие5/Домашнее_задание/task1/main.py
+++ Занятие5/Домашнее_задание/task1/main.py
@@ -1,12 +1,12 @@
 if __name__ == "__main__":
-
+    n = 6
     matrix = [
-        [i * j for j in range(1,10)]
-        for i in range(1,10)
+        [i * j for j in range(1, n+1)]
+        for i in range(1, n+1)
     ]
 
-    for row in range(1,10):
-        for col in range(1,10):
+    for row in range(1, n+1):
+        for col in range(1, n+1):
             print(f"{row * col:2}", end=" ")
         print()
 
