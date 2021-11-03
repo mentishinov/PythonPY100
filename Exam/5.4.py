3f39a5909f646e563bb0ab1c056938905b2125b2
Алексей <mentishinov@ya.ru>
2021-11-03 13:35:08 +0300

    подготовка к зачету

---
+++ Exam/5.4.py
@@ -0,0 +1,13 @@
+# с предусловием
+# i = 10
+# while i >= 10 and i <= 30:
+#     print(i)
+#     i+=1
+# с постусловием
+i = 10
+while True:
+    if i >= 10 and i <= 30:
+        print(i)
+        i+=1
+    else:
+        break
---
+++ Exam/7.1.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    n = 7
+    matrix = [
+        [i * j for j in range(1, n+1)]
+        for i in range(1, n+1)
+    ]
+
+    for row in range(1, n+1):
+        for col in range(1, n+1):
+            print(f"{row * col:2}", end=" ")
+        print()
